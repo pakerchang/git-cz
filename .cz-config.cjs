80f519fc826e95b85dee8c7107f97ffb1d39c2a9
pakerchang <pakerchang@gmail.com>
2023-07-25 14:09:10 +0800

    ref(config): Update ruls, scope.

--- .cz-config.cjs
+++ .cz-config.cjs
@@ -2,10 +2,10 @@ module.exports = {
   types: [
     { value: "feat", name: "feat: 增加" },
     { value: "fix", name: "fix: bug fix" },
-    { value: "style", name: "style: 樣式修改(不影響)" },
+    { value: "style", name: "style: 樣式修改(不影響邏輯)" },
     { value: "docs", name: "docs: 修改文件" },
     { value: "test", name: "test: 新增或修改現有的測試" },
-    { value: "refactor", name: "refactor: 重構" },
+    { value: "ref", name: "refactor: 重構" },
     {
       value: "chore",
       name: "chore: 修改建置流程、包管理、構建過程或輔助工具的變動。不包含修改測試檔、src 裡的檔案",
@@ -13,7 +13,7 @@ module.exports = {
     { value: "revert", name: "revert: 版本回退" },
     { value: "del", name: "del: 刪除" },
   ],
-  scopes: [],
+  scopes: ["component", "files", "style", "utils", "config", "other"],
   messages: {
     type: "選擇更改類型:\n",
     scope: "選擇 scope（Options）：\n",
